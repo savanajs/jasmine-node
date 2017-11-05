4b5372e7da5340ce50e556e746abc8ef311e93fa
Rafael Cruz <rafaelcruz1@hotmail.com>
2017-11-05 19:30:38 -0200

    fix: Readme20

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-    plugins: [ 'karma-jasmine', 'karma-chrome-launcher'],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
